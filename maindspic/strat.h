f83362f95f22587e76c1c01ec4746492eeb5e85f
Silvia Santano <SilviaSantano@users.noreply.github.com>
2014-01-19 20:37:50 +0100

    Strat: Added position of the tokens in the field, defined zones, modified strat_infos structure with information about the new zones and tokens and state of the robot variables, removed old variables and structures from 2012 in .h. Filled the whole strat_infos structure in .c.

--- maindspic/strat.h
+++ maindspic/strat.h
@@ -36,71 +36,58 @@
 //#define OBS_CLERANCE  205.0    /* when goes backwards */
 
 /* position of the tokens */
-#define TOTEM_1_X            1100
-#define TOTEM_1_Y            (AREA_Y/2)
-#define TOTEM_2_X            1900
-#define TOTEM_2_Y            (AREA_Y/2)
-#define TOTEM_WIDE           250
-
-#define OUR_TOTEM_X				TOTEM_1_X
-#define OUR_TOTEM_Y 				TOTEM_1_Y
-#define OPP_TOTEM_X 				TOTEM_2_X
-#define OPP_TOTEM_Y 				TOTEM_2_Y
-
-#define OPP_FLOOR_COIN_1_X   2000 
-#define OPP_FLOOR_COIN_1_Y   500 
-#define OPP_FLOOR_COIN_2_X   2150 
-#define OPP_FLOOR_COIN_2_Y   1000 
-#define OPP_FLOOR_COIN_3_X   2550 
-#define OPP_FLOOR_COIN_3_Y   1700
- 
-#define OUR_FLOOR_COIN_1_X   1000 
-#define OUR_FLOOR_COIN_1_Y   500 
-#define OUR_FLOOR_COIN_2_X   850 
-#define OUR_FLOOR_COIN_2_Y   1000 
-#define OUR_FLOOR_COIN_3_X   450
-#define OUR_FLOOR_COIN_3_Y   1700  
-
-#define BOTTLES_Y            2000
-#define OUR_BOTTLE_1_X       640  
-#define OUR_BOTTLE_2_X       1883  
-#define OPP_BOTTLE_1_X       2360   
-#define OPP_BOTTLE_2_X       1117   
-
-#define MIDDLE_COINS_GROUP_X  	1500
-#define MIDDLE_COINS_GROUP_Y     1700
-
-#define MIDDLE_FLOOR_GOLDBAR_X  1500
-#define MIDDLE_FLOOR_GOLDBAR_Y  1353
-
-#define OUR_FLOOR_GOLDBAR_X  		420
-#define OUR_FLOOR_GOLDBAR_Y  		785
-
-#define OPP_FLOOR_GOLDBAR_X     	2580
-#define OPP_FLOOR_GOLDBAR_Y     	785
-
-#define OUR_SHIP_DECK_1_X        200
-#define OUR_SHIP_DECK_1_Y        900
-#define OUR_SHIP_DECK_2_X        200
-#define OUR_SHIP_DECK_2_Y        900
-
-#define OPP_SHIP_DECK_1_X        (3000-200)
-#define OPP_SHIP_DECK_1_Y        900
-#define OPP_SHIP_DECK_2_X        (3000-200)
-#define OPP_SHIP_DECK_2_Y        900
-
-#define OUR_CAPTAINS_BEDROOM_X	250
-#define OUR_CAPTAINS_BEDROOM_Y	250
-#define OPP_CAPTAINS_BEDROOM_X	2750
-#define OPP_CAPTAINS_BEDROOM_Y	250
-
-#define SAVE_TREASURE_X				700
-#define SAVE_TREASURE_Y				1400
-
-#define OUR_MAP_X						1500
-#define OUR_MAP_Y							0
-
-
+#define TREE_1_X			3000
+#define TREE_1_Y			1300
+#define TREE_2_X			2300
+#define TREE_2_Y			2000
+#define TREE_3_X			700
+#define TREE_3_Y			2000
+#define TREE_4_X			0
+#define TREE_4_Y			1300
+#define HEART_FIRE_1_X		2860
+#define HEART_FIRE_1_Y		1860
+#define HEART_FIRE_2_X		1500
+#define HEART_FIRE_2_Y		1050
+#define HEART_FIRE_3_X		140
+#define HEART_FIRE_3_Y		1860
+#define FIRE_1_X			2600
+#define FIRE_1_Y			1100
+#define FIRE_2_X			2100
+#define FIRE_2_Y			600
+#define FIRE_3_X			2100
+#define FIRE_3_Y			1600
+#define FIRE_4_X			900
+#define FIRE_4_Y			600
+#define FIRE_5_X			900
+#define FIRE_5_Y			1600
+#define FIRE_6_X			400
+#define FIRE_6_Y			1100
+#define TORCH_1_X			3000
+#define TORCH_1_Y			800
+#define TORCH_2_X			1700
+#define TORCH_2_Y			2000
+#define TORCH_3_X			1300
+#define TORCH_3_Y			2000
+#define TORCH_4_X			0
+#define TORCH_4_Y			800
+#define MOBILE_TORCH_1_X		2100
+#define MOBILE_TORCH_1_Y		1100
+#define MOBILE_TORCH_2_X		900
+#define MOBILE_TORCH_2_Y		1100
+#define BASKET_1_X		2300
+#define BASKET_1_Y		150
+#define BASKET_2_X		700
+#define BASKET_2_Y		150
+#define MAMOOTH_1_X		2300
+#define MAMOOTH_1_Y		0
+#define MAMOOTH_2_X		700
+#define MAMOOTH_2_Y		0
+#define FRESCO_X			1500
+#define FRESCO_X			0
+#define HOME_RED_X		200
+#define HOME_RED_Y		300
+#define HOME_YELLOW_X		2800
+#define HOME_YELLOW_Y		300
 
 /* convert coords according to our color */
 #define COLOR_Y(y)     (y)
@@ -158,53 +145,41 @@
 #define SPEED_DIST_VERY_SLOW 	500
 #define SPEED_ANGLE_VERY_SLOW 500
 
+// 2011 ??
 #define PLACE_D_SAFE	 140
-
-/*for coins */
-#define ONE       0
-#define GROUP     1
 
 
 /* zones */
-#define ZONE_TOTEM_OUR_SIDE_1			   0
-#define ZONE_TOTEM_OUR_SIDE_2       	1
-#define ZONE_TOTEM_OPP_SIDE_1			   2
-#define ZONE_TOTEM_OPP_SIDE_2			   3
-#define ZONE_OUR_FLOOR_COIN_1			   4
-#define ZONE_OUR_FLOOR_COIN_2  	      5
-#define ZONE_OUR_FLOOR_COIN_3 	      6
-#define ZONE_OUR_FLOOR_GOLDBAR   	   7
-#define ZONE_OPP_FLOOR_COIN_1	         8
-#define ZONE_OPP_FLOOR_COIN_2	         9
-#define ZONE_OPP_FLOOR_COIN_3	         10
-#define ZONE_OPP_FLOOR_GOLDBAR			11
-#define ZONE_OUR_BOTTLE_1			      12
-#define ZONE_OUR_BOTTLE_2   			   13
-#define ZONE_OPP_BOTTLE_1			      14
-#define ZONE_OPP_BOTTLE_2			      15
-#define ZONE_OUR_MAP          	      16
-#define ZONE_OPP_MAP          	      17
-#define ZONE_MIDDLE_COINS_GROUP        18
-#define ZONE_MIDDLE_FLOOR_GOLDBAR      19
+#define ZONE_TREE_1			   0
+#define ZONE_TREE_2       	1
+#define ZONE_TREE_3			   2
+#define ZONE_TREE_4			   3
+#define ZONE_HEART_FIRE_1			   4
+#define ZONE_HEART_FIRE_2  	      5
+#define ZONE_HEART_FIRE_3 	      6
+#define ZONE_FIRE_1   	   7
+#define ZONE_FIRE_2	         8
+#define ZONE_FIRE_3	         9
+#define ZONE_FIRE_4	         10
+#define ZONE_FIRE_5			11
+#define ZONE_FIRE_6			      12
+#define ZONE_TORCH_1   			   13
+#define ZONE_TORCH_2			      14
+#define ZONE_TORCH_3			      15
+#define ZONE_TORCH_4          	      16
+#define ZONE_MOBILE_TORCH_1          	      17
+#define ZONE_MOBILE_TORCH_2        18
+#define ZONE_MOBILE_TORCH_3      19
+#define ZONE_BASKET_1      20
+#define ZONE_BASKET_2      21
+#define ZONE_MAMOOTH_1      22
+#define ZONE_MAMOOTH_2      23
+#define ZONE_FRESCO      	 24
+#define ZONE_HOME_RED      25
+#define ZONE_HOME_YELLOW      26
 
-/* place zones */
-#define ZONE_SHIP_OUR_CAPTAINS_BEDRROM	20
-#define ZONE_SHIP_OUR_HOLD	            21
-#define ZONE_SHIP_OUR_DECK_2           22
-#define ZONE_SHIP_OUR_DECK_1        	23
+#define ZONES_MAX		27
 
-#define ZONE_SHIP_OPP_CAPTAINS_BEDRROM	24
-#define ZONE_SHIP_OPP_HOLD             25
-#define ZONE_SHIP_OPP_DECK_1          	26
-#define ZONE_SHIP_OPP_DECK_2        	27
-
-#define ZONE_SAVE_TREASURE		         28
-#define ZONES_MAX								29
-
-/* Store goldbars and coins or not and where */
-#define STORE_MOUTH    1
-#define STORE_BOOT     2
-#define DONT_STORE     0
 
 /************************************************************* 
  * Strat data structures 
@@ -233,17 +208,16 @@ struct conf {
 typedef struct {
 	/* type */
 	uint16_t type;
-	#define ZONE_TYPE_TOTEM					0
-	#define ZONE_TYPE_GOLDBAR				1
-	#define ZONE_TYPE_MAP					2
-	#define ZONE_TYPE_BOTTLE				3
-	#define ZONE_TYPE_COIN					4
-	#define ZONE_TYPE_COINS_GROUP			5
-	#define ZONE_TYPE_HOLD					6
-	#define ZONE_TYPE_DECK					7
-	#define ZONE_TYPE_CAPTAINS_BEDROOM	8
-	#define ZONE_TYPE_SAVE					9
-	#define ZONE_TYPE_MAX					10
+	#define ZONE_TYPE_TREE					0
+	#define ZONE_TYPE_FIRE					1
+	#define ZONE_TYPE_HEART_FIRE				2
+	#define ZONE_TYPE_TORCH					3
+	#define ZONE_TYPE_MOBILE_TORCH				4
+	#define ZONE_TYPE_FRESCO					5
+	#define ZONE_TYPE_MAMOOTH					6
+	#define ZONE_TYPE_BASKET					7
+	#define ZONE_TYPE_HOME					8
+	#define ZONE_TYPE_MAX					9
 
 	/* target point */
 	int16_t x;
@@ -251,17 +225,11 @@ typedef struct {
 
 	/* boundinbox */
 	int16_t x_down;
-	int16_t y_up;
 	int16_t x_up;
 	int16_t y_down;
+	int16_t y_up;
 
-
-	//int16_t x_up;
-	//int16_t y_up;
-	//int16_t x_down;
-	//int16_t y_down;
-
-   /* init point */
+   	/* init point */
 	int16_t init_x;
 	int16_t init_y;
 
@@ -302,21 +270,18 @@ struct strat_infos {
 	uint8_t goto_zone;
 	uint8_t last_zone;
 
-	/* step of each empty totem */
-	uint8_t step_our_totem_1;
-	uint8_t step_our_totem_2;
-	uint8_t step_opp_totem_1;
-	uint8_t step_opp_totem_2;
-
 	/* opponent zone position */
 	uint8_t opp_current_zone;
 	uint8_t opp2_current_zone;
 
-	/* opponent stadistics */
+	/* opponent statistics */
 	uint32_t opp_time_zone_ms;
 
-	uint8_t treasure_in_mouth;
-	uint8_t treasure_in_boot;
+	/* state of the robot */
+	uint8_t fruits_inside;
+	uint8_t spears_inside;
+	uint8_t net_inside;
+	uint8_t fires_inside;
 };
 
 extern struct strat_infos strat_infos;
@@ -360,25 +325,6 @@ void strat_event(void *dummy);
  *******************************************/
  /* TODO 2014*/
 
-/********************************************
- * in strat_treasure.c 
- *******************************************/
-#ifdef old
-uint8_t strat_empty_totem_side(int16_t x, int16_t y, uint8_t store_goldbar, uint8_t step);
-uint8_t strat_pickup_coins_floor(int16_t x, int16_t y, uint8_t group);
-uint8_t strat_pickup_goldbar_floor(int16_t x, int16_t y, uint8_t store);
-uint8_t strat_send_message_bottle(int16_t x, int16_t y);
-uint8_t strat_save_treasure_generic(int16_t x, int16_t y);
-uint8_t strat_save_treasure_arms(int16_t x, int16_t y, uint8_t arm_side);
-uint8_t strat_save_treasure_in_deck_back(int16_t x, int16_t y);
-uint8_t strat_save_treasure_in_deck_back_blowing(int16_t x, int16_t y);
-uint8_t strat_save_treasure_in_hold_back(int16_t x, int16_t y);
-uint8_t strat_raise_window(uint8_t window);
-uint8_t strat_steal_treasure_hold(void);
-uint8_t strat_game(void);
-uint8_t strat_stole_opp_treasure(int16_t x, int16_t y);
-#endif
-
 
 /********************************************
  * in strat_main.c 
@@ -398,8 +344,6 @@ uint8_t strat_work_on_zone(uint8_t zone_num);
 /* smart play */
 //#define DEBUG_STRAT_SMART
 uint8_t strat_smart(void);
-
-/* add here more strat functions in files */
 
 #endif /* notyet TODO 2014 */
 
