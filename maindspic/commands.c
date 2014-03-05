3f834b194dbf84f1eca92bc8d8a0076fee63f375
supernudo <supernudo@arc-robots.org>
2014-03-05 21:47:22 +0100

    bt_protocol: wt11 multiplexing mode (sim test OK)

--- maindspic/commands.c
+++ maindspic/commands.c
@@ -108,6 +108,7 @@ extern parse_pgm_inst_t cmd_clitoid;
 extern parse_pgm_inst_t cmd_time_monitor;
 extern parse_pgm_inst_t cmd_strat_event;
 extern parse_pgm_inst_t cmd_sleep;
+extern parse_pgm_inst_t cmd_wt11;
 extern parse_pgm_inst_t cmd_gotozone;
 extern parse_pgm_inst_t cmd_workonzone;
 
@@ -230,6 +231,7 @@ parse_pgm_ctx_t main_ctx[] = {
 	(parse_pgm_inst_t *)&cmd_time_monitor,
 	(parse_pgm_inst_t *)&cmd_strat_event,
 	(parse_pgm_inst_t *)&cmd_sleep,
+	(parse_pgm_inst_t *)&cmd_wt11,
 	(parse_pgm_inst_t *)&cmd_gotozone,
 	(parse_pgm_inst_t *)&cmd_workonzone,
 
