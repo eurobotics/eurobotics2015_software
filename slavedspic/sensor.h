f6d26f1ac3f1f4d208cab08d71407238b0e3b46d
supernudo <supernudo@arc-robots.org>
2014-06-05 10:12:30 +0200

    XXX unstable code (do not compile) maindspic: strat fires functions improvements

--- slavedspic/sensor.h
+++ slavedspic/sensor.h
@@ -16,7 +16,7 @@
  *  along with this program; if not, write to the Free Software
  *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *
- *  Revision : $Id: sensor.h,v 1.4 2009/04/24 19:30:42 zer0 Exp $
+ *  Revision : $Id$
  *
  */
 
