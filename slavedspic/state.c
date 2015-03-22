5e84193a6536427c8db6c6f7ba63b7abe3af44a0
resaj <resaj@localhost>
2015-03-22 16:02:12 +0100

    Actuator cup_clamp_popcorn_door changed. Multiple actuator popcorn created.

--- slavedspic/state.c
+++ slavedspic/state.c
@@ -282,6 +282,8 @@ void state_do_stands_tower_clamps_mode(void)
 /* set cup_clamp_popcorn_door mode */
 void state_do_cup_clamp_popcorn_door_mode(void)
 {
+	uint8_t err = 0;
+
 	/* return if no update */
 	if (!state_check_update(CUP_CLAMP_POPCORN_DOOR))
 		return;
@@ -289,11 +291,23 @@ void state_do_cup_clamp_popcorn_door_mode(void)
 	state_set_status(I2C_SLAVEDSPIC_STATUS_BUSY);
 
 	/* set cup_clamp_popcorn_door mode */
-	if(cup_clamp_popcorn_door_set_mode(&slavedspic.cup_clamp_popcorn_door, mainboard_command.cup_clamp_popcorn_door.mode, mainboard_command.cup_clamp_popcorn_door.offset))
-		STMCH_ERROR("ERROR %s mode=%d", __FUNCTION__, state_get_mode());
+	if(mainboard_command.cup_clamp_popcorn_door.type == I2C_CUP_CLAMP_POPCORN_DOOR_TYPE_LEFT) {
+		if(cup_clamp_popcorn_door_set_mode(&slavedspic.cup_clamp_popcorn_door_l, mainboard_command.cup_clamp_popcorn_door.mode, mainboard_command.cup_clamp_popcorn_door.offset))
+			STMCH_ERROR("ERROR %s mode=%d", __FUNCTION__, state_get_mode());
 
-	cup_clamp_popcorn_door_wait_end(&slavedspic.cup_clamp_popcorn_door);
+		err = stands_elevator_wait_end(&slavedspic.stands_elevator_l);
+	}
+	else if(mainboard_command.cup_clamp_popcorn_door.type == I2C_CUP_CLAMP_POPCORN_DOOR_TYPE_RIGHT) {
+		if(cup_clamp_popcorn_door_set_mode(&slavedspic.cup_clamp_popcorn_door_r, mainboard_command.cup_clamp_popcorn_door.mode, mainboard_command.cup_clamp_popcorn_door.offset))
+			STMCH_ERROR("ERROR %s mode=%d", __FUNCTION__, state_get_mode());
 
+		err = stands_elevator_wait_end(&slavedspic.stands_elevator_r);
+	}
+
+	if(err & END_BLOCKING)
+		STMCH_DEBUG("CUP_CLAMP_POPCORN_DOOR mode ends with BLOCKING");
+
+	/* notice status and update mode*/
 	state_set_status(I2C_SLAVEDSPIC_STATUS_READY);
 }
 
@@ -386,48 +400,203 @@ void popcorn_system_init(popcorn_system_t *ps, uint8_t cup_front_sensor, uint8_t
 
 void popcorn_system_manage(popcorn_system_t *ps)
 {
+	static microseconds us = 0;
+	static uint8_t flag_tray_open = 0;
+	static uint8_t flag_ramps_open = 0;
+	static uint8_t flag_clamp_door_l_open = 0;
+	static uint8_t flag_clamp_door_r_open = 0;
+
 	/* update mode */
 	if(ps->mode_changed){
 		ps->mode_changed = 0;
 		ps->mode = ps->mode_rqst;
-//		ps->state = 0;
 		STMCH_DEBUG("%s mode=%d", __FUNCTION__, ps->mode);
 	}
 
 	switch(ps->mode)
 	{
 		case I2C_SLAVEDSPIC_MODE_PS_IDLE:
-
 			break;
+
 		case I2C_SLAVEDSPIC_MODE_PS_CUP_FRONT_CATCH_AND_DROP:
+			cup_clamp_front_set_mode(&slavedspic.cup_clamp_front, CUP_CLAMP_FRONT_MODE_CUP_LOCKED, 0);
+
+			if(cup_clamp_front_test_traj_end(&slavedspic.cup_clamp_front) & (END_NEAR|END_TRAJ)) {
+				ps->cup_front_catched = 1;
+				cup_holder_front_set_mode(&slavedspic.cup_holder_front, CUP_HOLDER_FRONT_MODE_CUP_HOLD, 0);
+			}
+
+			if(ps->cup_front_catched && (cup_holder_front_test_traj_end(&slavedspic.cup_holder_front) & (END_NEAR|END_TRAJ)))
+				us = time_get_us2();
+
+			if((time_get_us2() - us > 500000L) && ps->cup_front_catched)
+				ps->mode = I2C_SLAVEDSPIC_MODE_PS_IDLE;
 
 			break;
+
 		case I2C_SLAVEDSPIC_MODE_PS_CUP_FRONT_RELEASE:
+			cup_holder_front_set_mode(&slavedspic.cup_holder_front, CUP_HOLDER_FRONT_MODE_HIDE, 0);
+
+			if(cup_holder_front_test_traj_end(&slavedspic.cup_holder_front) & (END_NEAR|END_TRAJ)) {
+				ps->cup_front_catched = 0;
+				cup_clamp_front_set_mode(&slavedspic.cup_clamp_front, CUP_CLAMP_FRONT_MODE_HIDE, 0);
+			}
+
+			if(!ps->cup_front_catched && (cup_clamp_front_test_traj_end(&slavedspic.cup_clamp_front) & (END_NEAR|END_TRAJ)))
+					ps->mode = I2C_SLAVEDSPIC_MODE_PS_IDLE;
 
 			break;
+
 		case I2C_SLAVEDSPIC_MODE_PS_CUP_REAR_CATCH:
+			if(flag_clamp_door_l_open != 2 || flag_clamp_door_r_open != 2) {
+				ps->mode = I2C_SLAVEDSPIC_MODE_PS_IDLE;
+				return;
+			}
+
+			cup_clamp_set_mode(&slavedspic.cup_clamp_popcorn_door_l, CUP_CLAMP_MODE_LOCKED, 0);
+			cup_clamp_set_mode(&slavedspic.cup_clamp_popcorn_door_r, CUP_CLAMP_MODE_LOCKED, 0);
+
+			if((cup_clamp_popcorn_door_test_traj_end(&slavedspic.cup_clamp_popcorn_door_l) & (END_NEAR|END_TRAJ)) &&
+					(cup_clamp_popcorn_door_test_traj_end(&slavedspic.cup_clamp_popcorn_door_r) & (END_NEAR|END_TRAJ))) {
+				ps->cup_rear_catched = 1;
+				flag_clamp_door_l_open = 1;
+				flag_clamp_door_r_open = 1;
+				ps->mode = I2C_SLAVEDSPIC_MODE_PS_IDLE;
+			}
 
 			break;
+
 		case I2C_SLAVEDSPIC_MODE_PS_CUP_REAR_RELEASE:
+			if(!flag_clamp_door_l_open && !flag_clamp_door_r_open) {
+				popcorn_door_set_mode(&slavedspic.cup_clamp_popcorn_door_r, CUP_CLAMP_MODE_OPEN, 0);
+				flag_clamp_door_r_open = 2;
+				us = time_get_us2();
+			}
+			else if(!flag_clamp_door_l_open && flag_clamp_door_r_open) {
+				if(time_get_us2() - us > 300000L) {
+					popcorn_door_set_mode(&slavedspic.cup_clamp_popcorn_door_l, CUP_CLAMP_MODE_OPEN, 0);
+					flag_clamp_door_l_open = 2;
+				}
+			}
+			else {
+				popcorn_door_set_mode(&slavedspic.cup_clamp_popcorn_door_l, CUP_CLAMP_MODE_OPEN, 0);
+				popcorn_door_set_mode(&slavedspic.cup_clamp_popcorn_door_r, CUP_CLAMP_MODE_OPEN, 0);
+				flag_clamp_door_l_open = 2;
+				flag_clamp_door_r_open = 2;
+			}
+
+			if((cup_clamp_popcorn_door_test_traj_end(&slavedspic.cup_clamp_popcorn_door_l) & (END_NEAR|END_TRAJ)) &&
+					(cup_clamp_popcorn_door_test_traj_end(&slavedspic.cup_clamp_popcorn_door_r) & (END_NEAR|END_TRAJ))) {
+				ps->cup_rear_catched = 0;
+				ps->mode = I2C_SLAVEDSPIC_MODE_PS_IDLE;
+			}
 
 			break;
+
 		case I2C_SLAVEDSPIC_MODE_PS_MACHINES_READY:
+			if(!flag_tray_open) {
+				popcorn_tray_set_mode(&slavedspic.popcorn_tray, POPCORN_TRAY_MODE_OPEN, 0);
+				us = time_get_us2();
+				flag_ramps_open = 0;
+				flag_tray_open = 1;
+			}
+
+			if((time_get_us2() - us > 300000L) && flag_tray_open && !flag_ramps_open) {
+				popcorn_ramps_set_mode(&slavedspic.popcorn_ramps, POPCORN_RAMPS_MODE_OPEN, 0);
+				flag_ramps_open = 1;
+			}
+
+			if((popcorn_ramps_test_traj_end(&slavedspic.popcorn_ramps) & (END_NEAR|END_TRAJ)) && flag_ramps_open)
+				ps->mode = I2C_SLAVEDSPIC_MODE_PS_IDLE;
 
 			break;
+
 		case I2C_SLAVEDSPIC_MODE_PS_MACHINES_HARVEST:
+			if(!flag_tray_open || !flag_ramps_open) {
+				ps->mode = I2C_SLAVEDSPIC_MODE_PS_IDLE;
+				return;
+			}
+			else
+			{
+				popcorn_ramps_set_mode(&slavedspic.popcorn_ramps, POPCORN_RAMPS_MODE_HARVEST, 0);
+
+				if((popcorn_ramps_test_traj_end(&slavedspic.popcorn_ramps) & (END_NEAR|END_TRAJ))) {
+					us = time_get_us2();
+					ps->machine_popcorns_catched = 1;
+				}
+
+				if((time_get_us2() - us > 500000L) && ps->machine_popcorns_catched)
+					ps->mode = I2C_SLAVEDSPIC_MODE_PS_MACHINES_END;
+			}
 
 			break;
+
 		case I2C_SLAVEDSPIC_MODE_PS_MACHINES_END:
+			popcorn_ramps_set_mode(&slavedspic.popcorn_ramps, POPCORN_RAMPS_MODE_HIDE, 0);
 
+			if(popcorn_ramps_test_traj_end(&slavedspic.popcorn_ramps) & (END_NEAR|END_TRAJ)) {
+				flag_ramps_open = 0;
+				popcorn_tray_set_mode(&slavedspic.popcorn_tray, POPCORN_TRAY_MODE_CLOSE, 0);
+				flag_tray_open = 0;
+				ps->mode = I2C_SLAVEDSPIC_MODE_PS_IDLE;
+			}
 			break;
+
 		case I2C_SLAVEDSPIC_MODE_PS_STOCK_DROP:
+			if(!flag_clamp_door_l_open && !flag_clamp_door_r_open) {
+				popcorn_door_set_mode(&slavedspic.cup_clamp_popcorn_door_r, POPCORN_DOOR_MODE_OPEN, 0);
+				flag_clamp_door_r_open = 2;
+				us = time_get_us2();
+			}
+			else if(!flag_clamp_door_l_open && flag_clamp_door_r_open) {
+				if(time_get_us2() - us > 300000L) {
+					popcorn_door_set_mode(&slavedspic.cup_clamp_popcorn_door_l, POPCORN_DOOR_MODE_OPEN, 0);
+					flag_clamp_door_l_open = 2;
+				}
+			}
+			else {
+				popcorn_door_set_mode(&slavedspic.cup_clamp_popcorn_door_l, POPCORN_DOOR_MODE_OPEN, 0);
+				popcorn_door_set_mode(&slavedspic.cup_clamp_popcorn_door_r, POPCORN_DOOR_MODE_OPEN, 0);
+				flag_clamp_door_l_open = 2;
+				flag_clamp_door_r_open = 2;
+			}
+
+			if((cup_clamp_popcorn_door_test_traj_end(&slavedspic.cup_clamp_popcorn_door_l) & (END_NEAR|END_TRAJ)) &&
+					(cup_clamp_popcorn_door_test_traj_end(&slavedspic.cup_clamp_popcorn_door_r) & (END_NEAR|END_TRAJ))) {
+				ps->cup_rear_catched = 0;
+				ps->machine_popcorns_catched = 0;
+				us = time_get_us2();
+			}
+
+			if((time_get_us2() - us > 1000000L) && !ps->machine_popcorns_catched)
+				ps->mode = I2C_SLAVEDSPIC_MODE_PS_IDLE;
 
 			break;
-		case I2C_SLAVEDSPIC_MODE_PS_STOCK_END:
 
+		case I2C_SLAVEDSPIC_MODE_PS_STOCK_END:
+			if(ps->cup_rear_catched) {
+				ps->mode = I2C_SLAVEDSPIC_MODE_PS_IDLE;
+				return;
+			}
+				
+			popcorn_door_set_mode(&slavedspic.cup_clamp_popcorn_door_l, POPCORN_DOOR_MODE_OPEN, 0);
+
+			if((cup_clamp_popcorn_door_test_traj_end(&slavedspic.cup_clamp_popcorn_door_l) & (END_NEAR|END_TRAJ))) {
+				flag_clamp_door_l_open = 0;				
+				popcorn_door_set_mode(&slavedspic.cup_clamp_popcorn_door_r, POPCORN_DOOR_MODE_OPEN, 0);
+			}
+
+			if((cup_clamp_popcorn_door_test_traj_end(&slavedspic.cup_clamp_popcorn_door_l) & (END_NEAR|END_TRAJ)) && !flag_clamp_door_l_open) {
+				flag_clamp_door_r_open = 0;
+				ps->mode = I2C_SLAVEDSPIC_MODE_PS_IDLE;
+			}
+
+			break;
+
+		default:
+			ps->mode = I2C_SLAVEDSPIC_MODE_PS_IDLE;
 			break;
 	}
-
 }
 
 void state_do_popcorn_system(void)
@@ -454,29 +623,34 @@ void stands_system_manage(stands_system_t *ss, stands_system_t *ss_slave)
 	if(ss->mode_changed){
 		ss->mode_changed = 0;
 		ss->mode = ss->mode_rqst;
-		ss->mode = 0;
 		STMCH_DEBUG("%s mode=%d", __FUNCTION__, ss->mode);
 	}
 
 	switch(ss->mode)
 	{
 		case I2C_SLAVEDSPIC_MODE_SS_IDLE:
-
 			break;
+
 		case I2C_SLAVEDSPIC_MODE_SS_HIDE:
 
 			break;
+
 		case I2C_SLAVEDSPIC_MODE_SS_HARVESTING:
 
 			break;
+
 		case I2C_SLAVEDSPIC_MODE_SS_BUILD_SPOTLIGHT:
 
 			break;
+
 		case I2C_SLAVEDSPIC_MODE_SS_RELEASE_SPOTLIGHT:
 
 			break;
-	}
 
+		default:
+			ss->mode = I2C_SLAVEDSPIC_MODE_SS_IDLE;
+			break;
+	}
 }
 
 void state_do_stands_systems(void)
@@ -566,7 +740,8 @@ void state_init(void)
 
 	stands_tower_clamps_set_mode(&slavedspic.stands_tower_clamps, STANDS_TOWER_CLAMPS_MODE_LOCK, 0);
 
-	cup_clamp_popcorn_door_set_mode(&slavedspic.cup_clamp_popcorn_door, CUP_CLAMP_MODE_HIDE, 0);
+	cup_clamp_popcorn_door_set_mode(&slavedspic.cup_clamp_popcorn_door_l, CUP_CLAMP_MODE_HIDE, 0);
+	cup_clamp_popcorn_door_set_mode(&slavedspic.cup_clamp_popcorn_door_r, CUP_CLAMP_MODE_HIDE, 0);
 
 	popcorn_tray_set_mode(&slavedspic.popcorn_tray, POPCORN_TRAY_MODE_CLOSE, 0);
 	popcorn_ramps_set_mode(&slavedspic.popcorn_ramps, POPCORN_RAMPS_MODE_HIDE, 0);
