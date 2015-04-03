aced0e065aefd0f1db83b87d093efcfaec16d4d4
Javier Baliñas Santos <supernudo@users.noreply.github.com>
2015-04-03 23:07:04 +0200

    Near range opponent sensors and cup sensor setup XXX new Omron sensor need Rpullup > 100k for a proper L value.

---
+++ maindspic/strat_main.c
@@ -0,0 +1,639 @@
+/*  
+ *  Copyright Robotics Association of Coslada, Eurobotics Engineering (2011)
+ * 
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ *  Revision : $Id$
+ *
+ *  Javier Bali�as Santos <javier@arc-robots.org> and Silvia Santano
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+#include <aversive/pgmspace.h>
+#include <aversive/queue.h>
+#include <aversive/wait.h>
+#include <aversive/error.h>
+
+#include <uart.h>
+#include <dac_mc.h>
+#include <pwm_servo.h>
+#include <clock_time.h>
+
+#include <pid.h>
+#include <quadramp.h>
+#include <control_system_manager.h>
+#include <trajectory_manager.h>
+#include <vect_base.h>
+#include <lines.h>
+#include <polygon.h>
+#include <obstacle_avoidance.h>
+#include <blocking_detection_manager.h>
+#include <robot_system.h>
+#include <position_manager.h>
+
+
+#include <rdline.h>
+#include <parse.h>
+
+#include "../common/i2c_commands.h"
+#include "i2c_protocol.h"
+#include "main.h"
+#include "strat.h"
+#include "strat_base.h"
+#include "strat_avoid.h"
+#include "strat_utils.h"
+#include "sensor.h"
+#include "actuator.h"
+#include "beacon.h"
+#include "cmdline.h"
+#include "bt_protocol.h"
+
+
+#define ERROUT(e) do {\
+		err = e;			 \
+		goto end;		 \
+	} while(0)
+
+
+/* Add here the main strategy, the intelligence of robot */
+
+
+/*
+ *Check if the zone is available. 
+ * @Param zone_num checked zone
+ * @Return zone | -1.
+ */
+ 
+
+int8_t strat_is_valid_zone(uint8_t zone_num)
+{
+//#define OPP_WAS_IN_ZONE_TIMES	
+
+	//static uint16_t opp_times[ZONES_MAX];
+	//static microseconds opp_time_us = 0;
+
+	/* discard current zone */
+	if(strat_infos.current_zone == zone_num)
+	{
+		//DEBUG(E_USER_STRAT,"zone num: %d. current_zone.");
+		return -1;	
+	}
+
+	/* discard if opp is in zone */
+	if(opponents_are_in_area(COLOR_X(strat_infos.zones[zone_num].x_up), strat_infos.zones[zone_num].y_up,
+								  COLOR_X(strat_infos.zones[zone_num].x_down),	strat_infos.zones[zone_num].y_down)) {		
+		return -1;
+	}
+
+	/* discard avoid and checked zones */
+	if(strat_infos.zones[zone_num].flags & ZONE_AVOID)
+	{
+		//DEBUG(E_USER_STRAT,"zone num: %d. avoid.");
+		return -1;	
+	}
+		
+	return zone_num;
+}
+void set_next_sec_strategy(){
+	strat_infos.current_sec_strategy++;
+	switch(strat_infos.current_sec_strategy){
+		case 1:
+			set_strat_sec_1();
+			break;
+		case 2:
+			set_strat_sec_2();
+			break;
+		case 3:
+			set_strat_sec_3();
+			strat_infos.current_sec_strategy = 0;
+			break;
+		default:
+			break;
+	}
+	
+}
+void set_strat_main_1(void){
+	strat_infos.zones[ZONE_MY_STAND_GROUP_1].prio = 100;
+	strat_infos.zones[ZONE_POPCORNCUP_2].prio = 90;
+	strat_infos.zones[ZONE_MY_STAND_GROUP_2].prio = 80;
+	strat_infos.zones[ZONE_MY_CLAP_1].prio = 70;
+	strat_infos.zones[ZONE_MY_CLAP_2].prio = 60;
+	strat_infos.zones[ZONE_MY_STAND_GROUP_3].prio = 50;
+	strat_infos.zones[ZONE_MY_POPCORNMAC].prio = 40;
+	strat_infos.zones[ZONE_MY_STAND_GROUP_4].prio = 30;
+	strat_infos.zones[ZONE_MY_HOME].prio = 30;
+}
+
+
+void set_strat_sec_1(void){
+	strat_infos.current_sec_strategy = 1;
+	DEBUG(E_USER_STRAT,"strat_sec_1");
+	strat_infos.zones[ZONE_MY_CLAP_3].prio = 100;
+	strat_infos.zones[ZONE_MY_CINEMA_DOWN].prio = 90;
+	strat_infos.zones[ZONE_MY_CINEMA_UP].prio = 80;
+	strat_infos.zones[ZONE_MY_STAIRS].prio = 70;
+}
+void set_strat_sec_2(void){
+	strat_infos.current_sec_strategy = 2;
+	DEBUG(E_USER_STRAT,"strat_sec_2");
+	strat_infos.zones[ZONE_MY_CINEMA_UP].prio = 100;
+	strat_infos.zones[ZONE_MY_CLAP_3].prio = 90;
+	strat_infos.zones[ZONE_MY_CINEMA_DOWN].prio = 80;	
+	strat_infos.zones[ZONE_MY_STAIRS].prio = 70;
+}
+void set_strat_sec_3(void){
+	strat_infos.current_sec_strategy = 3;
+	DEBUG(E_USER_STRAT,"strat_sec_3");
+	strat_infos.zones[ZONE_MY_CINEMA_DOWN].prio = 100;
+	strat_infos.zones[ZONE_MY_CLAP_3].prio = 90;
+	strat_infos.zones[ZONE_MY_CINEMA_UP].prio = 80;
+	strat_infos.zones[ZONE_MY_STAIRS].prio = 70;
+}
+
+
+/* return new work zone, -1 if any zone is found */
+int8_t strat_get_new_zone(void)
+{
+	uint8_t prio_max = 0;
+	int8_t zone_num = -1, valid_zone= -1 ;
+	int8_t i=0;
+	/* evaluate zones */
+	for(i=0; i < ZONES_MAX; i++) 
+	{
+		/* compare current priority */
+		if(strat_infos.zones[i].prio >= prio_max && (strat_infos.zones[i].flags != ZONE_CHECKED)) {
+			/* check if is a valid zone */
+			prio_max = strat_infos.zones[i].prio;
+			zone_num = i;
+		}
+		if( i == ZONES_MAX-1 ){
+			valid_zone = strat_is_valid_zone(zone_num);
+			switch(valid_zone){
+				case -1:
+					//strategy has to change
+					set_next_sec_strategy(); 
+					i=0;
+					break;
+				default:
+					break;
+			}
+		}
+
+	}
+	if(zone_num == -1){
+		DEBUG(E_USER_STRAT,"No more zones available");
+	}
+	return zone_num;
+}
+
+/* return END_TRAJ if zone is reached, err otherwise */
+uint8_t strat_goto_zone(uint8_t zone_num)
+{
+	int8_t err=0;
+
+	
+	/* update strat_infos */
+	strat_infos.current_zone=-1;
+	strat_infos.goto_zone=zone_num;
+	
+	/* Secondary robot */
+	if(strat_infos.zones[zone_num].robot==SEC_ROBOT)
+	{
+		/*bt_robot_2nd_goto_and_avoid(COLOR_X(strat_infos.zones[zone_num].init_x), 
+												strat_infos.zones[zone_num].init_y);
+												
+		err = bt_robot_2nd_wait_end();*/
+		//Temporally 2scondary robot is not ready to test.
+		err = goto_and_avoid (COLOR_X(strat_infos.zones[zone_num].init_x),
+										strat_infos.zones[zone_num].init_y,  
+										TRAJ_FLAGS_STD, TRAJ_FLAGS_NO_NEAR);
+		
+	}else{
+		err = goto_and_avoid (COLOR_X(strat_infos.zones[zone_num].init_x),
+										strat_infos.zones[zone_num].init_y,  
+										TRAJ_FLAGS_STD, TRAJ_FLAGS_NO_NEAR);
+
+	}
+	
+	/* update strat_infos */
+	strat_infos.last_zone=strat_infos.current_zone;
+	strat_infos.goto_zone=-1;
+	
+	if (!TRAJ_SUCCESS(err)) {
+		strat_infos.current_zone=-1;
+	}
+	else{
+		strat_infos.current_zone=zone_num;
+	}
+end:
+    /* TODO XXX if error put arm in safe position */
+	return err;
+}
+
+
+/* return END_TRAJ if the work is done, err otherwise */
+uint8_t strat_work_on_zone(uint8_t zone_num)
+{
+	uint8_t err = END_TRAJ;
+	uint16_t temp_spdd, temp_spda;
+
+#ifdef HOST_VERSION
+	DEBUG(E_USER_STRAT,"strat_work_on_zone %d %s: press a key",zone_num,numzone2name[zone_num]);
+	while(!cmdline_keypressed());
+#endif
+
+
+	/* Secondary robot */
+	if(strat_infos.zones[zone_num].robot==SEC_ROBOT)
+	{
+	}
+
+	/* Main robot */
+	/*XXX TEST*/
+	else
+	{
+	    if(strat_infos.zones[zone_num].type!=ZONE_TYPE_STAND && strat_infos.zones[zone_num].type!=ZONE_TYPE_POPCORNCUP){
+			/* turn to wall */
+			trajectory_turnto_xy (&mainboard.traj, COLOR_X(strat_infos.zones[zone_num].x), strat_infos.zones[zone_num].y);
+			err = wait_traj_end (TRAJ_FLAGS_SMALL_DIST);
+			if (!TRAJ_SUCCESS(err))
+					ERROUT(err);
+
+			trajectory_d_rel (&mainboard.traj, 100);
+			err = wait_traj_end(TRAJ_FLAGS_NO_NEAR);
+			if (!TRAJ_SUCCESS(err))
+				ERROUT(err);
+			time_wait_ms(1000);
+			trajectory_d_rel (&mainboard.traj, -100);
+			err = wait_traj_end(TRAJ_FLAGS_NO_NEAR);
+			if (!TRAJ_SUCCESS(err))
+				ERROUT(err);
+			time_wait_ms(1000);
+				
+            }
+						/*ZONE_MY_STAND_GROUP_1*/
+			if(strat_infos.zones[zone_num].type == ZONE_MY_STAND_GROUP_1){
+			
+				err= goto_and_avoid (COLOR_X(MY_STAND_4_X),
+									MY_STAND_4_Y,  
+									TRAJ_FLAGS_STD, TRAJ_FLAGS_STD);
+				err= goto_and_avoid (COLOR_X(MY_STAND_5_X),
+									MY_STAND_5_Y,  
+									TRAJ_FLAGS_STD, TRAJ_FLAGS_STD);
+				err= goto_and_avoid (COLOR_X(MY_STAND_6_X),
+									LIMIT_BBOX_Y_DOWN,  
+									TRAJ_FLAGS_STD, TRAJ_FLAGS_STD);
+				if (!TRAJ_SUCCESS(err)){
+					ERROUT(err);
+				}
+
+			}
+			
+	}
+
+	end:
+	return err;
+}
+
+
+/* debug state machines step to step */
+void state_debug_wait_key_pressed(void)
+{
+	if (!strat_infos.debug_step)
+		return;
+
+	DEBUG(E_USER_STRAT,"press a key");
+	while(!cmdline_keypressed());
+}
+
+
+void recalculate_priorities(void)
+{
+	#if 0
+	uint8_t zone_num;
+	
+	for(zone_num=0; zone_num<ZONES_MAX; zone_num++)
+	{
+		/* 1. opp checked zone AFTER us */
+		if((strat_infos.zones[zone_num].flags & ZONE_CHECKED_OPP)&&
+		(strat_infos.zones[zone_num].flags & ZONE_CHECKED))
+		{
+				//TODO
+		}
+		
+		/* 2. checked zone */
+		else if(strat_infos.zones[zone_num].flags & ZONE_CHECKED)
+		{
+				//TODO
+		}
+		
+		/* 3. Points we can get now in this zone */
+		switch(strat_infos.zones[zone_num].type)
+		{
+			case ZONE_TYPE_HEART:
+				if(strat_infos.zones[zone_num].flags & ZONE_CHECKED_OPP)
+					strat_zones_points[zone_num]=4;
+					
+				/* The robot has fires inside */
+				strat_zones_points[zone_num]+=(strat_infos.fires_inside*2);
+				break;
+				
+			case ZONE_TYPE_TREE:
+				/* If visited by the opponent: no points */
+				if(strat_infos.zones[zone_num].flags & ZONE_CHECKED_OPP)
+					strat_zones_points[zone_num]=0;
+				break;
+				
+			case ZONE_TYPE_FIRE:
+				if(strat_infos.zones[zone_num].flags & ZONE_CHECKED_OPP)
+					strat_zones_points[zone_num]=0;
+				break;
+				
+			case ZONE_TYPE_TORCH:
+				if(strat_infos.zones[zone_num].flags & ZONE_CHECKED_OPP)
+					strat_zones_points[zone_num]=0;
+				break;
+				
+			case ZONE_TYPE_M_TORCH:
+				if(strat_infos.zones[zone_num].flags & ZONE_CHECKED_OPP)
+					strat_zones_points[zone_num]=0;
+				break;
+				
+				
+				
+			case ZONE_TYPE_BASKET:
+				/* Save fruits on basket */
+				strat_zones_points[zone_num]=strat_infos.harvested_trees*3;
+				
+				/* Opponent has given us toxic fruits */
+				// XXX Remove toxic fruits available???
+				if(((mainboard.our_color==I2C_COLOR_YELLOW) && (zone_num==ZONE_BASKET_2)) ||
+				((mainboard.our_color==I2C_COLOR_GREEN) && (zone_num==ZONE_BASKET_1)))
+				{
+					if(strat_infos.zones[zone_num].flags & ZONE_CHECKED_OPP)
+					{
+						if(strat_infos.zones[ZONE_TREE_1].flags & ZONE_CHECKED_OPP)
+							strat_zones_points[zone_num]+=2;
+						if(strat_infos.zones[ZONE_TREE_2].flags & ZONE_CHECKED_OPP)
+							strat_zones_points[zone_num]+=2;
+						if(strat_infos.zones[ZONE_TREE_3].flags & ZONE_CHECKED_OPP)
+							strat_zones_points[zone_num]+=2;
+						if(strat_infos.zones[ZONE_TREE_4].flags & ZONE_CHECKED_OPP)
+							strat_zones_points[zone_num]+=2;
+					}
+				}
+				break;
+				
+			/* Remain the same */
+			case ZONE_TYPE_FRESCO:
+			case ZONE_TYPE_MAMOOTH:
+			case ZONE_TYPE_HOME:
+				break;
+				
+			default:
+				break;
+		}
+		
+		/* 4. Distance from robot to the zone */
+		// TODO
+		//Give points to closest zones
+		
+		
+		
+		/* Recalculate priorities depending on strategy TODO*/
+		/***********************************************************/
+		/* Defensive: protect our points */
+		
+		/* Risky: try to get the maximum points without protecting */
+		
+		/* Moderate: combine defense and risk */
+		
+		/* Offensive: make the opponent lose points */
+		/***********************************************************/
+	}
+	#endif
+}
+
+
+/* smart play */
+uint8_t strat_smart(void)
+{
+	int8_t zone_num;
+	uint8_t err;
+	
+	/* Tasks secondary robot */
+	
+	/* get new zone */
+	
+	zone_num = strat_get_new_zone();
+	DEBUG(E_USER_STRAT,"Zone: %d. Priority: %d",zone_num,strat_infos.zones[zone_num].prio);
+		
+	if(zone_num == -1) {
+		//DEBUG(E_USER_STRAT,"No zone is found");
+		return END_TRAJ;
+	}else{
+		/* goto zone */
+		//DEBUG(E_USER_STRAT,"Going to zone %s.",numzone2name[zone_num]);
+		strat_infos.goto_zone = zone_num;
+		strat_dump_infos(__FUNCTION__);
+		
+		err = strat_goto_zone(zone_num);
+		
+		if (!TRAJ_SUCCESS(err)) {
+			DEBUG(E_USER_STRAT,"Can't reach zone %d.", zone_num);
+			set_next_sec_strategy(); 
+			return END_TRAJ;
+		}
+		
+		DEBUG(E_USER_STRAT,"strat_work_on_zone");
+		/* work on zone */
+		strat_infos.last_zone = strat_infos.current_zone;
+		strat_infos.current_zone = strat_infos.goto_zone;
+		strat_dump_infos(__FUNCTION__);
+
+		err = strat_work_on_zone(zone_num);
+		if (!TRAJ_SUCCESS(err)) {
+		    DEBUG(E_USER_STRAT,"Work on zone %s fails.", numzone2name[zone_num]);
+                    /* IMPORTANT: If in home, get out of it before continuing */
+                    if(strat_infos.zones[zone_num].type==ZONE_TYPE_HOME)
+                    {
+                        do{
+                                err = goto_and_avoid (COLOR_X(strat_infos.zones[zone_num].init_x),  strat_infos.zones[zone_num].init_y,  TRAJ_FLAGS_STD, TRAJ_FLAGS_NO_NEAR);
+                        }while(!TRAJ_SUCCESS(err));
+                    }
+		}
+		else
+		{
+			// Switch off devices, go back to normal state if anything was deployed
+		}
+
+		// mark the zone as checked 
+		strat_infos.zones[zone_num].flags |= ZONE_CHECKED;
+
+		return END_TRAJ;
+	}
+}
+
+
+void strat_opp_tracking (void) 
+{
+#if 0
+#define MAX_TIME_BETWEEN_VISITS_MS	4000
+#define TIME_MS_TREE				1500
+#define TIME_MS_HEART				1500
+#define TIME_MS_BASKET				1000
+#define UPDATE_ZONES_PERIOD_MS		25	
+	
+	uint8_t flags;
+	uint8_t zone_opp;
+	
+    /* check if there are opponents in every zone */
+    for(zone_opp = 0; zone_opp <  ZONES_MAX-1; zone_opp++)
+    {
+	   
+	if(opponents_are_in_area(COLOR_X(strat_infos.zones[zone_opp].x_up), strat_infos.zones[zone_opp].y_up,
+                                     COLOR_X(strat_infos.zones[zone_opp].x_down), strat_infos.zones[zone_opp].y_down)){
+			
+			if(!(strat_infos.zones[zone_opp].flags & (ZONE_CHECKED_OPP)))
+			{
+				IRQ_LOCK(flags);
+				strat_infos.zones[zone_opp].last_time_opp_here=time_get_us2();
+				IRQ_UNLOCK(flags);
+				if((time_get_us2() - strat_infos.zones[zone_opp].last_time_opp_here) < MAX_TIME_BETWEEN_VISITS_MS*1000L)
+				{
+					/* Opponent continues in the same zone: */
+					/* update zone time */ 
+					IRQ_LOCK(flags);
+					strat_infos.zones[zone_opp].opp_time_zone_us += UPDATE_ZONES_PERIOD_MS*1000L;
+					IRQ_UNLOCK(flags);
+
+					/* Mark zone as checked and sum points */
+					switch(strat_infos.zones[zone_opp].type)
+					{
+						case ZONE_TYPE_TREE:
+							if(strat_infos.zones[zone_opp].opp_time_zone_us>=TIME_MS_TREE*1000L)
+							{
+								strat_infos.zones[zone_opp].flags |= ZONE_CHECKED_OPP;
+								strat_infos.opp_harvested_trees++;
+								DEBUG(E_USER_STRAT,"opp_harvested_trees=%d",strat_infos.opp_harvested_trees);
+								DEBUG(E_USER_STRAT,"OPP approximated score: %d", strat_infos.opp_score);
+							}
+							break;
+						case ZONE_TYPE_BASKET:
+							if(((mainboard.our_color==I2C_COLOR_YELLOW) && (zone_opp==ZONE_BASKET_1)) ||
+							((mainboard.our_color==I2C_COLOR_GREEN) && (zone_opp==ZONE_BASKET_2)))
+							{
+								if(strat_infos.zones[zone_opp].opp_time_zone_us>=TIME_MS_BASKET*1000L)
+								{
+									if(strat_infos.opp_harvested_trees!=0)
+									{
+										strat_infos.opp_score += strat_infos.opp_harvested_trees * 3;
+										strat_infos.opp_harvested_trees=0;
+										DEBUG(E_USER_STRAT,"opp_harvested_trees=%d",strat_infos.opp_harvested_trees);
+										DEBUG(E_USER_STRAT,"OPP approximated score: %d", strat_infos.opp_score);
+									}
+								}
+							}
+							break;
+						case ZONE_TYPE_HEART:
+							if(strat_infos.zones[zone_opp].opp_time_zone_us>= TIME_MS_HEART*1000L)
+							{
+								strat_infos.zones[zone_opp].flags |= ZONE_CHECKED_OPP;
+								strat_infos.opp_score += 4;
+								DEBUG(E_USER_STRAT,"OPP approximated score: %d", strat_infos.opp_score);
+							}
+							break;
+						default:
+							break;
+					}
+				}
+							
+				/* Zone has changed */
+				else
+				{
+					/* reset zone time */
+					IRQ_LOCK(flags);
+					strat_infos.zones[zone_opp].opp_time_zone_us = 0;
+					IRQ_UNLOCK(flags);
+				}
+			}
+		}
+	}
+#endif
+}
+
+
+void strat_homologation(void)
+{
+	uint8_t err;
+	uint8_t i=0;
+        #define ZONES_SEQUENCE_LENGTH 3
+	uint8_t zones_sequence[ZONES_SEQUENCE_LENGTH]={ZONE_MY_STAND_GROUP_1,ZONE_MY_POPCORNMAC,ZONE_MY_HOME};
+
+
+	
+	/* Secondary robot */
+	//
+	
+	time_wait_ms(2000);
+	trajectory_d_rel(&mainboard.traj,250);
+	err = wait_traj_end(TRAJ_FLAGS_SMALL_DIST);
+	
+	for(i=0; i<ZONES_SEQUENCE_LENGTH; i++)
+	{
+		/* goto zone */
+		//DEBUG(E_USER_STRAT,"Going to zone %s.",numzone2name[zones_sequence[i]]);
+		strat_dump_infos(__FUNCTION__);
+		strat_infos.current_zone=-1;
+		strat_infos.goto_zone=i;
+
+		strat_goto_zone (zones_sequence[i]);
+		err = wait_traj_end(TRAJ_FLAGS_STD);
+		if (!TRAJ_SUCCESS(err)) {
+			strat_infos.current_zone=-1;
+			DEBUG(E_USER_STRAT,"Can't reach zone %s.", numzone2name[zones_sequence[i]]);
+		}
+		else{
+			strat_infos.current_zone=i;
+		}
+
+		strat_infos.last_zone=strat_infos.current_zone;
+		strat_infos.goto_zone=-1;
+
+
+		/* work on zone */
+		strat_dump_infos(__FUNCTION__);
+		err = strat_work_on_zone(zones_sequence[i]);
+		if (!TRAJ_SUCCESS(err)) {
+                    DEBUG(E_USER_STRAT,"Work on zone %s fails.", numzone2name[zones_sequence[i]]);
+
+                    /* IMPORTANT: If in home, get out of it before continuing */
+                    if(strat_infos.zones[zones_sequence[i]].type==ZONE_TYPE_HOME)
+                    {
+                        do{
+                                err = goto_and_avoid (COLOR_X(strat_infos.zones[zones_sequence[i]].init_x),  strat_infos.zones[zones_sequence[i]].init_y,  TRAJ_FLAGS_STD, TRAJ_FLAGS_NO_NEAR);
+                        }while(!TRAJ_SUCCESS(err));
+                    }
+		}
+
+		/* mark the zone as checked */
+		strat_infos.zones[i].flags |= ZONE_CHECKED;
+	}
+}
+
---
+++ secondary_robot/strat_main.c
@@ -0,0 +1,180 @@
+/*  
+ *  Copyright Robotics Association of Coslada, Eurobotics Engineering (2011)
+ * 
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ *  Revision : $Id$
+ *
+ *  Javier Bali?as Santos <javier@arc-robots.org> and Silvia Santano 
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+#include <aversive/pgmspace.h>
+#include <aversive/queue.h>
+#include <aversive/wait.h>
+#include <aversive/error.h>
+
+#include <uart.h>
+#include <pwm_mc.h>
+#include <pwm_servo.h>
+#include <clock_time.h>
+
+#include <pid.h>
+#include <quadramp.h>
+#include <control_system_manager.h>
+#include <trajectory_manager.h>
+#include <vect_base.h>
+#include <lines.h>
+#include <polygon.h>
+#include <obstacle_avoidance.h>
+#include <blocking_detection_manager.h>
+#include <robot_system.h>
+#include <position_manager.h>
+#include <scheduler.h>
+
+
+#include <rdline.h>
+#include <parse.h>
+
+#include "../common/i2c_commands.h"
+//#include "../common/bt_commands.h"
+
+#include "i2c_protocol.h"
+#include "main.h"
+#include "strat.h"
+#include "strat_base.h"
+#include "../maindspic/strat_avoid.h"
+#include "../maindspic/strat_utils.h"
+#include "sensor.h"
+#include "actuator.h"
+#include "beacon.h"
+#include "cmdline.h"
+
+
+#define ERROUT(e) do {\
+		err = e;			 \
+		goto end;		 \
+	} while(0)
+
+/* Add here the main strategic, the inteligence of robot */
+
+
+/* auto possition depending on color */
+void strat_auto_position (void)
+{
+#define AUTOPOS_SPEED_FAST 	1000
+#define BASKET_WIDTH		300
+
+	uint8_t err;
+	uint16_t old_spdd, old_spda;
+
+	/* save & set speeds */
+	interrupt_traj_reset();
+	strat_get_speed(&old_spdd, &old_spda);
+	strat_set_speed(AUTOPOS_SPEED_FAST, AUTOPOS_SPEED_FAST);
+
+	/* goto blocking to y axis */
+	trajectory_d_rel(&mainboard.traj, -200);
+	err = wait_traj_end(END_INTR|END_TRAJ|END_BLOCKING);
+	if (err == END_INTR)
+		goto intr;
+	wait_ms(100);
+
+	/* set y */
+	strat_reset_pos(DO_NOT_SET_POS, BASKET_WIDTH + ROBOT_CENTER_TO_BACK, 90);
+
+	/* prepare to x axis */
+	trajectory_d_rel(&mainboard.traj, 60);
+	err = wait_traj_end(END_INTR|END_TRAJ);
+	if (err == END_INTR)
+		goto intr;
+
+	trajectory_a_rel(&mainboard.traj, COLOR_A_REL(-90));
+	err = wait_traj_end(END_INTR|END_TRAJ);
+	if (err == END_INTR)
+		goto intr;
+
+
+	/* goto blocking to x axis */
+	trajectory_d_rel(&mainboard.traj, -700);
+	err = wait_traj_end(END_INTR|END_TRAJ|END_BLOCKING);
+	if (err == END_INTR)
+		goto intr;
+	wait_ms(100);
+	
+	/* set x and angle */
+	strat_reset_pos(COLOR_X(ROBOT_CENTER_TO_BACK), DO_NOT_SET_POS, COLOR_A_ABS(0));
+	
+	/* goto start position */
+	trajectory_d_rel(&mainboard.traj, 150);
+	err = wait_traj_end(END_INTR|END_TRAJ);
+	if (err == END_INTR)
+		goto intr;
+	wait_ms(100);
+	
+	/* restore speeds */	
+	strat_set_speed(old_spdd, old_spda);
+	return;
+
+intr:
+	strat_hardstop();
+	strat_set_speed(old_spdd, old_spda);
+}
+
+
+
+uint8_t pick_popcorn_cup(void)
+{
+	uint8_t ret=0;
+    printf_P(PSTR("pick_popcorn_cup\r\n"));
+    return ret;
+}
+
+
+uint8_t extend_carpet(void)
+{
+	uint8_t ret=0;
+    printf_P(PSTR("extend_carpet\r\n"));
+    return ret;
+}
+
+
+uint8_t climb_stairs(void)
+{
+	uint8_t ret=0;
+    printf_P(PSTR("climb_stairs\r\n"));
+    return ret;
+}
+
+
+uint8_t bring_cup_to_cinema(void)
+{
+	uint8_t ret=0;
+    printf_P(PSTR("bring_cup_to_cinema\r\n"));
+    return ret;
+}
+
+
+uint8_t close_clapperboard(void)
+{
+	uint8_t ret=0;
+    printf_P(PSTR("clapperboard\r\n"));
+    return ret;
+}
+
