4dbf08feb2274a9a1cd38c54e02e98d6656a5bfc
supernudo <supernudo@arc-robots.org>
2015-05-23 12:54:15 +0200

    BUG FIXED: conf flags.
    New dimension of stairs, 3 tries of calib, flag cup exchange,
    blocking zone secondary robot, sync after goto failure.

--- maindspic/commands_mainboard.c
+++ maindspic/commands_mainboard.c
@@ -506,9 +506,10 @@ static void cmd_start_parsed(void *parsed_result, void *data)
 	if (!strcmp_P(res->strategy, PSTR("qualification")))
     {
 		strat_infos.match_strategy=STR_QUALIFICATION;
-		strat_infos.conf.flags = CONF_FLAG_DO_TOWER;	
-		strat_infos.conf.flags = CONF_FLAG_DO_ESCAPE_UPPER_ZONE;	
-		//strat_infos.conf.flags = CONF_FLAG_DO_STAND_FAST_GROUP_1;	
+		strat_infos.conf.flags = 0;	
+		strat_infos.conf.flags |= CONF_FLAG_DO_TOWER;	
+		strat_infos.conf.flags |= CONF_FLAG_DO_ESCAPE_UPPER_ZONE;	
+		//strat_infos.conf.flags |= CONF_FLAG_DO_STAND_FAST_GROUP_1;	
     }
 
     else if (!strcmp_P(res->strategy, PSTR("base")))
